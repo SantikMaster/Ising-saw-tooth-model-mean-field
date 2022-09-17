11b608efa61c5bb1038cc89a591ff6bbd3150973
SantikMaster <SantikMaster@gmail.com>
2022-09-17 15:14:39 +0200

    refactored

--- main.cpp
+++ main.cpp
@@ -2,20 +2,17 @@
 #include <math.h>
 #include <stdio.h>
 
-// доделать суммирование по состояниям и вычисление стат суммы
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-using namespace std;
-	double m_S1 = 0, m_S2 = 0, m_S3 = 0;
-	double h = 0.0;
-	double T=0.1;
-	long double Hamiltonian[8];
-void HamiltonianFill();
-long double fn();
-void TextOut(string Text, int Number);
+void HamiltonianFill(long double *Hamiltonian, double h, double, double, double);
+long double fn(long double *Hamiltonian, double T);
+void TextOut(const std::string & Text, int Number);
 
 int main(int argc, char** argv) 
 {
+	double m_S1 = 0, m_S2 = 0, m_S3 = 0;
+	double h = 0.0;
+	double T=0.1;
+	long double Hamiltonian[8];	
+	
 	double m_S1_t = 0, m_S2_t = 0, m_S3_t = 0;
     double M=0, M2= 0;
 	char buf[256];
@@ -24,125 +21,90 @@ int main(int argc, char** argv)
 	long double fN=0, fN_temp = 0, fNOld = 0;
 	int ii, jj, kk;
 
-while (h < 5)
-{
+	while (h < 5)
+	{	
+	 	m_S1 = 0; 
+	 	m_S2 = 0;
+		m_S3 = 0;
+		HamiltonianFill(Hamiltonian, h, 0, 0, 0);
+		fN_temp = fn(Hamiltonian, T);
+		fNOld = fn(Hamiltonian, T);
 	
-	 m_S1 = 0; 
-	 m_S2 = 0;
-	 m_S3 = 0;
-	HamiltonianFill();
-	fN_temp = fn();
-	fNOld = fn();
-//		HamiltonianFill();
-//	fN_temp = fn();
-	//	cout << "              " << fN<< "              "<< fN_temp;
-	
-	for (ii = 0; ii<40; ii++)
-	{
-		m_S1 = (1-ii*0.05)/2;
-		for (jj = 0; jj<40; jj++)
+		for (ii = 0; ii<40; ii++)
 		{
-			m_S2 = (1-jj*0.05)/2;
-			for (kk = 0; kk<40; kk++)
+			m_S1 = (1-ii*0.05)/2;
+			for (jj = 0; jj<40; jj++)
 			{
-				m_S3 = (1-kk*0.05)/2;
-	//	cout << "1" ;
-					HamiltonianFill();
-					fN = fn();
-	//					cout << "              " << fN<< "              "<< fN_temp;
+				m_S2 = (1-jj*0.05)/2;
+				for (kk = 0; kk<40; kk++)
+				{
+					m_S3 = (1-kk*0.05)/2;
+					HamiltonianFill(Hamiltonian, h, m_S1, m_S2, m_S3);
+					fN = fn(Hamiltonian, T);
+
 					if (fN<fN_temp)
 					{
-					//	cout << "!!!!!!!\n\n\n\n\n" ; 
 						fN_temp = fN;
 						m_S1_t = m_S1;
 						m_S2_t = m_S2;
-						m_S3_t = m_S3;
-						
+						m_S3_t = m_S3;	
 					}
-					
-					
-  //   				cout << " "  << Hamiltonian [1];
-				
+				}
+			}		
+		}
+		std::cout << " \n";
+		std::cout << " "  << m_S1_t << " "  << m_S2_t << " "  << m_S3_t <<" \n  	";
 
-//	    cout << " \n  	";
-	}}}
-	 cout << " \n  \n\n	";
-	  cout << " \n  	";
-	cout << " "  << m_S1_t << " "  << m_S2_t << " "  << m_S3_t ;
-	cout << " \n  	";
-//	*/
-	M = m_S1_t + m_S2_t + m_S3_t ;
-	M2 = -((fNOld) - (fN_temp))/ 0.05;
+		M = m_S1_t + m_S2_t + m_S3_t ;
+		M2 = -((fNOld) - (fN_temp))/ 0.05;
 	
 	
-	cout << " M =  	"<< M << "\n";
-	sprintf(buf, "%4.4f    %4.4f   %4.4f   %4.4f    %4.4f    %4.4f   \n", h,  M, M2, m_S1_t , m_S2_t , m_S3_t  );
-	string ST(buf);	
+		std::cout << " M =  	"<< M << "\n";
+		sprintf(buf, "%4.4f    %4.4f   %4.4f   %4.4f    %4.4f    %4.4f   \n", h,  M, M2, m_S1_t , m_S2_t , m_S3_t  );
+		std::string ST(buf);	
 				
-	TextOut(ST, 0);
+		TextOut(ST, 0);
 	
-	fNOld =  fN_temp;
-	h +=0.05;
-}
-
-//getchar();	
+		fNOld =  fN_temp;
+		h +=0.05;
+	}	
 	return 0;
 }
-void TextOut(string Text, int Number)
+
+void TextOut(const std::string & Text, int Number)
 {
-
 	// #include <stdio.h>
+	FILE *stream;
 
-	FILE	 	*stream;
-	char str[10];
-
-
-	char Stri[25];
-
-	//   itoa(Number, Stri, 12);
-	//  string strs(Stri);
-
-	//   strcpy(str,("D/Data"+strs+".txt").c_str() ) ;
-
-//	sprintf(Stri, "Data.txt");
 	stream =
 		fopen("DataEnh.txt",  "a");
-fopen(Stri,  "a");
-
-	//	stream = fopen(Stri, , "rw");
-
-	//  "Data.txt"
-	char * p;
+ 
+	char *p;
 	p = (char *)Text.c_str();
 	if (stream != NULL)
 	{
 		fprintf(stream, p, "DataEnh.txt");
-//		fprintf(stream, p, Stri);
 		fclose(stream);
 	}
 	return;
 }
-long double fn()
+
+long double fn(long double *Hamiltonian, double T)
 {
 	int i;
 	long double F = 0, F1;
-		for (i = 0; i<8; i++)
-	{
-		F += exp(-Hamiltonian[i]/T);
-		
 	
-	//		cout << "       \n\n";  	
-	//		cout << "   "<<Hamiltonian[i]<< "       "  <<exp(-Hamiltonian[i]/T);
-	//		cout << "    " << F;  
-			
-		
+	for (i = 0; i<8; i++)
+	{
+		F += exp(-Hamiltonian[i]/T);	
     }
+    
 	F1 = T*log(F);
 	return F1;
 }
 
 
-void HamiltonianFill()
+void HamiltonianFill(long double *Hamiltonian, double h, double m_S1, double m_S2, double m_S3)
 {
 	int i, j, k, numbr;
 	double S1, S2, S3, H;
@@ -162,9 +124,7 @@ void HamiltonianFill()
 				H += -m_S1*m_S3 - 1/2*m_S1*m_S2 - 1/2*m_S2*m_S3;
 				H += S1*(m_S2+m_S3)+ S3*(m_S2+m_S1);
 				
-				Hamiltonian[numbr] = H;
-			//		cout << " "  << H;
-	       
+				Hamiltonian[numbr] = H;  
 			}
 
 		}
